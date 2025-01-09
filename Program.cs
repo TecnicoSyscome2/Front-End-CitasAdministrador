ba26370a443ae62b008800b859ec17ebaa930d05
Tecnico1Syscome <tecnico5syscome@gmail.com>
2025-01-09 10:48:49 -0600

    Actu Asesores

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-using Front_End_Citas.Administrador;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Front_End_Citas.Administrador;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5291/api/") });
 
 await builder.Build().RunAsync();
