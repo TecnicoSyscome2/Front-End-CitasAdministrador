ec33218dda6b3d68758600097dcd5501c6655505
Tecnico5Syscome <tecnico5syscome@gmail.com>
2025-01-10 10:31:38 -0600

    Mantenimiento Asesores, Horario

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Front_End_Citas.Administrador;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5291/api/") });
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("http://localhost:5291/api/")
+});
 
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
 await builder.Build().RunAsync();
