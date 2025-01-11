b3e2b9c2bc6057e1229515a9a6fd880f22fdf86f
TecnicoSyscome2 <tecnico2syscome@gmail.com>
2025-01-11 11:47:02 -0600

    avances con login de SyscomeCitas

---
+++ Model/AuthResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Front_End_Citas.Administrador.Model
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@ using Blazored.LocalStorage;
 using Front_End_Citas.Administrador;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Front_End_Citas.Administrador;
-using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Front_End_Citas.Administrador.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,10 +11,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("http://147.93.136.6:9011/api/")
-    BaseAddress = new Uri("http://192.168.1.35:9011/api/")
+   // BaseAddress = new Uri("http://147.93.136.6:9011/api/")
+    BaseAddress = new Uri("http://localhost:5291/api/")
 });
-
-builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<UserAuth>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationProvider>();
 await builder.Build().RunAsync();
